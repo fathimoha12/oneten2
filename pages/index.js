b755ce767434e506086433d009bd8834ccca93db
fathimoha12 <fathimhmmd418@gmail.com>
2026-07-15 21:40:32 +0300

    d

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,13 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700;800;900&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="/styles.css?v=20260715-next" />
+        <link rel="stylesheet" href="/styles.css?v=20260715-detail-fix" />
       </Head>
       <div id="root" />
       <Script src="https://unpkg.com/react@18/umd/react.production.min.js" strategy="beforeInteractive" />
       <Script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js" strategy="beforeInteractive" />
-      <Script src="/config.js?v=20260715-next" strategy="afterInteractive" />
-      <Script src="/app.js?v=20260715-next" strategy="afterInteractive" />
+      <Script src="/config.js?v=20260715-detail-fix" strategy="afterInteractive" />
+      <Script src="/app.js?v=20260715-detail-fix" strategy="afterInteractive" />
     </>
   );
 }
